84e4403dd78c172e2ad5b8cb5f4a268c17955bb4
JETYIN <494699974@qq.com>
2017-02-26 13:26:09 +0800

    update dcl

--- src/SigleTonPattern/DCLSigle.java
+++ src/SigleTonPattern/DCLSigle.java
@@ -1,5 +1,22 @@
 package SigleTonPattern;
 
 public class DCLSigle {
+	/** DOUBLE CHECKLOCK的单例模式，推荐采用，在效率上有比较出色的表现 **/
 
+	private static DCLSigle instance = null;
+
+	private DCLSigle() {
+	}
+
+	public static DCLSigle getinstance() {
+		if (instance == null) {
+			synchronized (DCLSigle.class) {
+				if (instance == null) {
+					instance = new DCLSigle();
+				}
+
+			}
+		}
+		return instance;
+	}
 }
